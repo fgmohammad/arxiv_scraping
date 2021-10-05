801c42782afa7cfd8fefc9e382b36e4e697ac5ac
Faizan Gohar Mohammad <faizangoharmohammad@MacBook-Pro.lan>
2021-10-05 06:19:37 -0400

    updated

--- 02-get_new_entries.py
+++ 02-get_new_entries.py
@@ -12,17 +12,18 @@ html = requests.get(url).text
 soup = BeautifulSoup(html, 'lxml')
 
 # LOAD THE EXISTING RECORD
-path = 'arxiv_astro-ph_all.dat'
+dir_path = os.path.dirname(os.path.realpath(__file__))
+filename = os.path.join(dir_path, 'arxiv_astro-ph_all.dat')
 href_papers = []
-if os.path.isfile(path):
-    with open(path, 'r') as ifile:
+if os.path.isfile(filename):
+    with open(filename, 'r') as ifile:
         href_papers = ifile.read().splitlines()
 
 # GET THE NEW ENTRIES
 href_new = get_papers(url, href_papers)
 
 if len(href_new) > 0:
-    with open(path, 'a') as ofile:
+    with open(filename, 'a') as ofile:
         for href in href_new:
             ofile.write(f'{href}\n')
 
